434d44d722b5989ea7b1fa87050bff27c4d5cecd
boycany <yurayurateiko@gmail.com>
2022-07-18 10:16:18 +0800

    ch3-vue basic

---
+++ src/components/Task.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <li>
+        <div><slot></slot></div>
+        <small>{{ task.dueDate }} @ {{ task.dueTime }}</small>
+        <button @click="deleteTask(idx)">X</button>
+      </li>
+  </div>
+</template>
+<script>
+    export default {
+      props: ['task', 'idx', 'deleteTask']
+    };
+</script>
+<style>
+
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,107 +1,55 @@
 <template>
   <q-page padding>
-    <button 
-      style="position: absolute; right: 10px"
-      @click="counter++"
-    >
-      {{ counter }}
-    </button>
-
-    <input 
-      v-model="message" 
-      @keyup.esc="clearMessage" 
-      @keyup.enter="alertMessage" 
-      v-autofocus
-      :class="{ 'error' : message.length > 15 }"
-      ref="messageInput"
-    />
-    <button @click="clearMessage">CLEAR</button>
-    <div>Length Now: {{ message.length }}</div>
-    <h5 
-      v-show="message.length"
-      class="border-grey"
-      :class="{ scale: isScale}"
-      @mouseenter="scaleWord" 
-      :style="errorStyle"
-    >
-      {{message}}
-    </h5>
-
-    <hr>
-
-    <p>Uppercase message: {{ messageUppercase }} </p>
-    <p>Lowercase message: {{ message | messageLowercase }}</p>
+    <ul>
+      <task 
+        v-for="(task, idx) in tasks"
+        :task="task"
+        :idx="idx"
+        :key="task.id"
+        :deleteTask="deleteTask"
+        >
+      {{ task.name }}
+      </task>
+    </ul>
   </q-page>
 </template>
 
 <script>
+  import Task from '../components/Task'
   export default {
     data(){
       return {
-        message: "Vue.js",
-        isScale: false,
-        counter: 0
-      }   
-    },
-    computed: {
-      // 雖然 messageUppercase 是 method，但我們將它放在computed屬性內，看成像是一個動態的變數
-      messageUppercase(){
-        console.log("messageUppercase is fire!");
-        return this.message.toUpperCase()
-      },
-      errorStyle(){
-        if(this.message.length > 15){
-          return {
-            'color': 'red',
-            'backgroundColor': 'pink' 
-          }
-        }
+        tasks: [
+        {
+          id: 1,
+          name: 'First Thing',
+          dueDate: '2018/05/12',
+          dueTime: '18:30'
+        }, 
+        {
+          id: 2,
+          name: 'Second Thing',
+          dueDate: '2018/05/12',
+          dueTime: '18:30'
+        },
+        {
+          id: 3,
+          name: 'Third Thing',
+          dueDate: '2018/05/12',
+          dueTime: '18:30'
+        }]
       }
-    },
+    }, 
     methods: {
-      scaleWord(){
-        this.isScale = !this.isScale
-      },
-      clearMessage(){
-        this.message = ""
-      },
-      alertMessage(){
-        alert(this.message)
-      },
-    },  
-    filters: {
-      messageLowercase(value){
-        console.log("messageLowercase is fire!");
-        return value.toLowerCase()
+      deleteTask(idx){
+        this.tasks.splice(idx, 1)
       }
     },
-    directives: {
-      autofocus:{
-        inserted(el){
-          el.focus()
-        }
-      }
-    },
-    mounted(){
-      console.log(this.$refs);
-      this.$refs.messageInput.className = "bg-green"
-    },
+    components: {
+      'task': Task
+    }
   }
 </script>
 <style lang="scss">
-  .scale{
-    transition-duration: 500ms;
-    font-size: 50px;
-    padding: 20px;
-  }
-  .border-grey{
-    border: 1px solid grey;
-  }
-  .error{
-    color: red;
-    background: pink;
-  }
-  input, button {
-    font-size: 23px;
-  } 
+ 
 </style>
